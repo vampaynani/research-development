d40e27c9a7916a44f8a095252ff83c7b45de6fb5
Vampaynani <vampay@digitaldealers.mx>
2019-06-26 20:37:50 -0500

    changes on deploy

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 JEKYLL_ENV=production jekyll b
-cd _site && git add . && git commit -m \"$(git log '--format=format:%H' master -1)\ && git push --force
+cd _site && git add . && git commit -m \"$(git log '--format=format:%H' master -1)\" && git push --force
